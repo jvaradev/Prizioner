c45c3dccd7726162dd73b6851a91d29c76bd785a
javivara100 <javier.varaselgas@educa.madrid.org>
2024-03-20 21:33:29 +0100

    Add movement and jump

--- My project/Assets/Script/PlayerMovement.cs
+++ My project/Assets/Script/PlayerMovement.cs
@@ -1,23 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-using UnityEngine;
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 2f;
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private bool isGrounded; 
 
-public class PlayerMovement : MonoBehaviour
-{
-    public float speed = 5f;
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-    
-    void Update()
-    {
-        
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        
-        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * speed * Time.deltaTime;
+    private void Update() {
+        Vector2 movement = Vector2.zero;
 
+        // Mover hacia la derecha con la tecla "d"
+        if (Input.GetKey("d")) {
+            movement += Vector2.right * speed * Time.deltaTime;
+        }
 
+        // Mover hacia la izquierda con la tecla "a"
+        if (Input.GetKey("a")) {
+            movement += Vector2.left * speed * Time.deltaTime;
+        }
+
+        // Aplicar el movimiento al objeto
         transform.Translate(movement);
+
+        // Saltar con la tecla de espacio si está en el suelo
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded ) {
+            Jump();
+        }
+    }
+
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    // Método llamado cuando el personaje colisiona con otro objeto
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // Colision con Tilemap
+        if (collision.gameObject.GetComponent<TilemapCollider2D>() != null) {
+            isGrounded = true;
+        }
+    }
+
+    // Método llamado cuando el personaje deja de colisionar con otro objeto
+    private void OnCollisionExit2D(Collision2D collision) {
+        // Colision con Tilemap
+        if (collision.gameObject.GetComponent<TilemapCollider2D>() != null) {
+            isGrounded = false;
+        }
     }
 }
