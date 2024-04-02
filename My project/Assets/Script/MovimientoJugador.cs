311f80829ad2f99e64f3115c13ed154daf79826f
javivara100 <javier.varaselgas@educa.madrid.org>
2024-04-02 21:57:26 +0200

    Add transition from Idle to Run

--- My project/Assets/Script/MovimientoJugador.cs
+++ My project/Assets/Script/MovimientoJugador.cs
@@ -1,36 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-
-    public float speed = 5f;
+    public float speed = 3f;
     public float jumpSpeed = 3f;
     private Rigidbody2D rb2D;
-    
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-    
+
     void FixedUpdate()
     {
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
+            animator.SetFloat("Horizontal", Mathf.Abs(speed));
             rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
+            spriteRenderer.flipX = false; // No invertir la imagen
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
+            animator.SetFloat("Horizontal", Mathf.Abs(speed));
             rb2D.velocity = new Vector2(-speed, rb2D.velocity.y);
+            spriteRenderer.flipX = true; // Invertir horizontalmente
         }
         else
         {
+            animator.SetFloat("Horizontal", Mathf.Abs(0));
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
         }
 
-        if (Input.GetKey("space")&&CheckGround.isGround)
+        if (Input.GetKey("space") && CheckGround.isGround)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
         }
