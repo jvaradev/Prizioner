a0d559bd3f632381ac35634d919c700c6a3563cf
javivara100 <javier.varaselgas@educa.madrid.org>
2024-04-08 21:41:31 +0200

    Add Enemy texture and Enemy Animation

--- My project/Assets/Script/PatrullaEnemigo.cs
+++ My project/Assets/Script/PatrullaEnemigo.cs
@@ -12,16 +12,19 @@ public class PatrullaEnemigo : MonoBehaviour
     [SerializeField] private float distanciaMinima;
     private int numAlt;
     private SpriteRenderer sr;
+    private Animator animator;
 
     private void Start()
     {
         numAlt = Random.Range(0, puntos.Length);
+        animator = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
         Girar();
     }
 
     private void Update()
     {
+        animator.SetBool("Walk",true);
         transform.position = Vector2.MoveTowards(transform.position, puntos[numAlt].position, velocidad * Time.deltaTime);
 
         if (Vector2.Distance(transform.position, puntos[numAlt].position)<distanciaMinima)
@@ -35,11 +38,11 @@ public class PatrullaEnemigo : MonoBehaviour
     {
         if (transform.position.x < puntos[numAlt].position.x)
         {
-            sr.flipX = true;
+            sr.flipX = false;
         }
         else
         {
-            sr.flipX = false;
+            sr.flipX = true;
         }
     }
 }
