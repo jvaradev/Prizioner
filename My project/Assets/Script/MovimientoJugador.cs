31da74658b072f4eb7cd15e9e4cf039479c6bc94
javivara100 <javier.varaselgas@educa.madrid.org>
2024-04-08 20:18:59 +0200

    Fix fall animation and refactor PlayerMovement

--- My project/Assets/Script/MovimientoJugador.cs
+++ My project/Assets/Script/MovimientoJugador.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 2f;
     public float jumpSpeed = 3f;
     private Rigidbody2D rb2D;
-    private Animator animator;
+    public Animator animator;
     private SpriteRenderer spriteRenderer;
 
     void Start()
@@ -19,27 +19,64 @@ public class MovimientoJugador : MonoBehaviour
 
     void FixedUpdate()
     {
+        Movement();
+        Jump();
+        Fall();
+    }
+
+    //Movimiento horizontal con animaciones
+    private void Movement()
+    {
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            animator.SetFloat("Horizontal", Mathf.Abs(speed));
+            animator.SetBool("Run", true);
             rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
             spriteRenderer.flipX = false; // No invertir la imagen
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
-        {
-            animator.SetFloat("Horizontal", Mathf.Abs(speed));
+        {            
+            animator.SetBool("Run", true);
             rb2D.velocity = new Vector2(-speed, rb2D.velocity.y);
             spriteRenderer.flipX = true; // Invertir horizontalmente
         }
         else
         {
-            animator.SetFloat("Horizontal", Mathf.Abs(0));
+            animator.SetBool("Run", false);
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
         }
+    }
 
+    //Movimiento salto con transición de animaciones
+    private void Jump()
+    {
         if (Input.GetKey("space") && CheckGround.isGround)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+        }
+
+        if (CheckGround.isGround==false)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        
+        if (CheckGround.isGround)
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("Fall", false);
+        }
+    }
+    
+    //Movimiento de caer con animaciones
+    private void Fall()
+    {
+        if (rb2D.velocity.y<0)
+        {
+            animator.SetBool("Fall", true);
+        }
+        else if (rb2D.velocity.y>0)
+        {
+            animator.SetBool("Fall", false);
         }
     }
 }
