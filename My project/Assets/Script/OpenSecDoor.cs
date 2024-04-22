879d9e00f7e906691120bb3fafea26b886c28b23
javivara100 <javier.varaselgas@educa.madrid.org>
2024-04-22 11:54:27 +0200

    Add Tutorial Open Doors

--- My project/Assets/Script/OpenSecDoor.cs
+++ My project/Assets/Script/OpenSecDoor.cs
@@ -2,22 +2,52 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement; 
 
 public class OpenSecDoor : MonoBehaviour
 {
     private Animator animator;
-    private float tiempoApertura = 1;
+    private Animator animatorKey;
+    private GameObject squareObject;
+    private float tiempoApertura = 2;
     private bool sceneChanged = false;
+    private bool stayDoor;
+    private void Start()
+    {
+        // Encuentra el objeto "Square" en la escena
+        squareObject = GameObject.Find("Square");
+        // Asegúrate de que esté desactivado al principio
+        squareObject.SetActive(false);
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         animator = GetComponent<Animator>();
-        if (collision.CompareTag("Player") && Input.GetKey("e") && !sceneChanged)
+        animatorKey = squareObject.GetComponentInChildren<Animator>();
+        if (collision.CompareTag("Player"))
         {
+            squareObject.SetActive(true);
+            squareObject.transform.localScale = new Vector2(4, 4);
+            animatorKey.SetBool("Start", true);
+            stayDoor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        squareObject.SetActive(false);
+        stayDoor = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey("e") && !sceneChanged && stayDoor)
+        {
+            Debug.Log("Tecla E presionada");
             animator.SetBool("Open", true);
             StartCoroutine(TiempoAbrirPuerta());
-        }       
+        }
     }
 
     public IEnumerator TiempoAbrirPuerta()
