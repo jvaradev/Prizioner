b01432c37dda2767ec31b237b4d08794863e51e2
javivara100 <javier.varaselgas@educa.madrid.org>
2024-04-22 20:20:51 +0200

    Add Crouch and CrouchWalk

--- My project/Assets/Script/MovimientoJugador.cs
+++ My project/Assets/Script/MovimientoJugador.cs
@@ -1,14 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-    public float speed = 2f;
-    public float jumpSpeed = 3f;
+    public float speed = 5f;
+    public float jumpSpeed = 4f;
     private Rigidbody2D rb2D;
     public Animator animator;
     private SpriteRenderer spriteRenderer;
+    private BoxCollider2D bc2D;
+    private Vector2 originalColliderSize; // Almacena el tamaño original del collider
+    private Vector2 originalColliderOffset; // Almacena el desplazamiento original del collider
     public bool sePuedeMover = true;
     [SerializeField] private Vector2 velocidadRebote;
 
@@ -17,6 +18,11 @@ public class MovimientoJugador : MonoBehaviour
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        bc2D = GetComponent<BoxCollider2D>();
+
+        // Almacenar el tamaño y el offset originales del collider
+        originalColliderSize = bc2D.size;
+        originalColliderOffset = bc2D.offset;
     }
 
     void FixedUpdate()
@@ -25,9 +31,9 @@ public class MovimientoJugador : MonoBehaviour
         {
             Movement();
             Jump();
-            Fall(); 
+            Fall();
+            Crouch();
         }
-        
     }
 
     //Movimiento horizontal con animaciones
@@ -40,7 +46,7 @@ public class MovimientoJugador : MonoBehaviour
             spriteRenderer.flipX = false; // No invertir la imagen
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
-        {            
+        {
             animator.SetBool("Run", true);
             rb2D.velocity = new Vector2(-speed, rb2D.velocity.y);
             spriteRenderer.flipX = true; // Invertir horizontalmente
@@ -55,32 +61,32 @@ public class MovimientoJugador : MonoBehaviour
     //Movimiento salto con transición de animaciones
     private void Jump()
     {
-        if (Input.GetKey("space") && CheckGround.isGround)
+        if (Input.GetKey("w") && CheckGround.isGround)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
         }
 
-        if (CheckGround.isGround==false)
+        if (CheckGround.isGround == false)
         {
             animator.SetBool("Jump", true);
             animator.SetBool("Run", false);
         }
-        
+
         if (CheckGround.isGround)
         {
             animator.SetBool("Jump", false);
             animator.SetBool("Fall", false);
         }
     }
-    
+
     //Movimiento de caer con animaciones
     private void Fall()
     {
-        if (rb2D.velocity.y<0)
+        if (rb2D.velocity.y < 0)
         {
             animator.SetBool("Fall", true);
         }
-        else if (rb2D.velocity.y>0)
+        else if (rb2D.velocity.y > 0)
         {
             animator.SetBool("Fall", false);
         }
@@ -89,5 +95,41 @@ public class MovimientoJugador : MonoBehaviour
     public void Rebote(Vector2 puntoGolpe)
     {
         rb2D.velocity = new Vector2(-velocidadRebote.x * puntoGolpe.x, velocidadRebote.y);
+    }
+
+    public void Crouch()
+    {
+        speed = 3f;
+        if (Input.GetKey("s") && CheckGround.isGround)
+        {
+            // Reducir el tamaño del collider
+            bc2D.size = new Vector2(originalColliderSize.x, originalColliderSize.y * 0.5f);
+            // Mover el collider hacia abajo
+            bc2D.offset = new Vector2(originalColliderOffset.x, originalColliderOffset.y - (originalColliderSize.y * 0.25f));
+            animator.SetBool("Crouch", true);
+            if (Input.GetKey("d"))
+            {
+                animator.SetBool("CrouchWalk", true);
+                rb2D.velocity = new Vector2(speed, rb2D.velocity.y);
+                spriteRenderer.flipX = false;
+            }
+            else if (Input.GetKey("a"))
+            {
+                animator.SetBool("CrouchWalk", true);
+                rb2D.velocity = new Vector2(-speed, rb2D.velocity.y);
+                spriteRenderer.flipX = true; // Invertir horizontalmente
+            }
+            else
+            {
+                animator.SetBool("CrouchWalk", false);
+            }
+        }
+        else
+        {
+            // Restaurar el tamaño y el offset originales del collider
+            bc2D.size = originalColliderSize;
+            bc2D.offset = originalColliderOffset;
+            animator.SetBool("Crouch", false);
+        }
     }
 }
