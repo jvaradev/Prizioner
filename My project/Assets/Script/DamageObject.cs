321aaaf20acd8457dbb6997eae96b0b9a963e0ab
javivara100 <javier.varaselgas@educa.madrid.org>
2024-04-25 11:37:33 +0200

    Fixed Health and Damage

--- My project/Assets/Script/DamageObject.cs
+++ My project/Assets/Script/DamageObject.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class DamageObject : MonoBehaviour
 {
+    [SerializeField] public float damage;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<CombatPlayer>().GetDamage(20, collision.GetContact(0).normal);
+            collision.gameObject.GetComponent<CombatPlayer>().GetDamage(damage/2, collision.GetContact(0).normal);
             collision.transform.GetComponent<PlayerRespawn>().PlayerDamaged();
         }
     }
